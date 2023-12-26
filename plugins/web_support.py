6ff2213e9b119569a21ad667f30b8d05bea63496
REYAD X SHIPU <129636176+BINOD-XD@users.noreply.github.com>
2023-12-26 21:16:14 +0600

    Add files via upload

---
+++ plugins/web_support.py
@@ -0,0 +1,17 @@
+# Don't Remove Credit @teamrxs
+# Follow Github For More Project BINOD-XD
+# Ask Doubt on telegram @mrboombastic4
+
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("LazyDeveloper")
+
+
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
